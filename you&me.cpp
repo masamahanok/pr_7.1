5aeec3991cb89980b1e48fe03ff209794e3716d3
masamahanok <142673416+masamahanok@users.noreply.github.com>
2026-05-21 11:04:18 +0300

    1

--- you&me.cpp
+++ you&me.cpp
@@ -1,32 +1,44 @@
 #include <stdio.h>
 int main(void) {
-	int n = 5, W = 10;
-	int wt[] = { 2,3,4,5,1 };
-	int val[] = { 3,4,5,6,1 };
-	printf("Предмети:\n");
-	printf("%-4s %-8s %-8s\n", "Nr", "Вага", "Цінність");
+	const int n = 5;                        //Кількість предметів
+	const int W = 10;                       //Місткість рюкзака
+	int wt[] = { 2,3,4,5,1 };               //Вага предметів
+	int val[] = { 3,4,5,6,1 };              //Цінність предметів
+	printf("Predmetu:\n");
+	printf("%-4s %-8s %-8s\n", "Nr", "Vaha", "Tsinnost");
 	for (int i = 0;i < n;i++)
 		printf("%-4d %-8d %-8d\n", i + 1, wt[i], val[i]);
-	printf("Місткість рюкзака: %d\n\n", W);
-	/* Таблиця DP */
+	printf("Mistkist ryukzaka: %d\n\n", W);
+	/* Tablytsya DP(dunamychne prohramuvannya) */
 	int dp[n + 1][W + 1];
 	for (int i = 0;i <= n;i++)
 		for (int w = 0;w <= W;w++) {
-			if (i == 0 || w == 0) { dp[i][w] = 0;continue; }
+			if (i == 0 || w == 0) { 
+				dp[i][w] = 0;
+				continue; 
+			}
 			dp[i][w] = dp[i - 1][w];
 			if (wt[i - 1] <= w) {
-				int v = val[i - 1] + dp[i - 1][w - wt[i - 1]];
+				int v = val[i - 1] + dp[i - 1][w - wt[i - 1]];//ціність попередніх предметів без ваги поточного
 				if (v > dp[i][w]) dp[i][w] = v;
 			}
 		}
-	printf("Максимальна цінність: %d\n", dp[n][W]);
-	/* Відновити список предметів */
-	printf("Вибрані предмети: ");
+
+	printf("--- TABLYTSYA DP ---\n");
+	for (int i = 0; i <= n; i++) {
+		for (int w = 0; w <= W; w++) {
+			printf("%-3d ", dp[i][w]);
+		}
+		printf("\n");
+	}
+
+	printf("Maksymalna tsinnost: %d\n", dp[n][W]);
+	/* Vidnovyty spysok predmetiv */
+	printf("Vybrani predmety: ");
 	int w = W;
 	for (int i = n;i > 0;i--)
 		if (dp[i][w] != dp[i - 1][w]) {
-			printf("Nr%d(вага=%d,цін=%d)
-				",i,wt[i-1],val[i-1]);
+			printf("Nr%d(vaha=%d,tsin=%d)",i,wt[i-1],val[i-1]);
 				w -= wt[i - 1];
 		}
 	printf("\n");
